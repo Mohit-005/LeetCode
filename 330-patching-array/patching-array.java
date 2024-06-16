7331e804d80a03c010eba7d2450e65ff3cab53e3
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-06-16 08:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (78.11%) - LeetSync

---
+++ 330-patching-array/patching-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        int patches = 0;
+        int i = 0;
+        long miss = 1;
+        while(miss <= n) {
+            if(i < nums.length && nums[i] <= miss)
+                miss += nums[i++];
+            else {
+                miss += miss;
+                patches++;
+            }
+        }
+        return patches;
+    }
+}
