4d800b77ea0db1452f994d68fae86aa5077b9db6
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-06-27 22:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 66.6 MB (88.07%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,10 +1,5 @@
 class Solution {
-    public int findCenter(int[][] edges) {
-        //the one that is common in sab
-        int u=edges[0][0];
-        int v=edges[0][1];
-        int x=edges[1][0];
-        int y=edges[1][1];
-        return (u==x || u==y?u:v);
-    }
+    public int findCenter(int[][] e) {
+    return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
+}
 }
