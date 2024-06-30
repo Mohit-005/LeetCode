fd63aad23b57298b4caaf9c5b81640a4f63618cc
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-06-30 05:57:22 +0530

    Time: 10 ms (96.64%) | Memory: 108.1 MB (21.01%) - LeetSync

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -3,67 +3,67 @@ class Solution {
         UnionFind alice = new UnionFind(n);
         UnionFind bob = new UnionFind(n);
 
-        
         int edgesRequired = 0;
-        for(int[] edge : edges) {
-            if(edge[0]==3) {
-                edgesRequired+= (alice.preformUnion(edge[1], edge[2]) | bob.preformUnion(edge[1], edge[2]));
+        // First process type 3 edges
+        for (int[] edge : edges) {
+            if (edge[0] == 3) {
+                edgesRequired += (alice.preformUnion(edge[1], edge[2]) | bob.preformUnion(edge[1], edge[2]));
             }
         }
 
-        for(int[] edge : edges) {
-            if(edge[0]==2) {
+        // Process type 2 and type 1 edges
+        for (int[] edge : edges) {
+            if (edge[0] == 2) {
                 edgesRequired += bob.preformUnion(edge[1], edge[2]);
-            } else if(edge[0]==1) {
+            } else if (edge[0] == 1) {
                 edgesRequired += alice.preformUnion(edge[1], edge[2]);
             }
         }
 
-        if(alice.isConnected() && bob.isConnected()) {
+        // Check if both Alice's and Bob's graphs are fully connected
+        if (alice.isConnected() && bob.isConnected()) {
             return edges.length - edgesRequired;
         }
 
         return -1;
-
     }
 
     class UnionFind {
-
         int[] representative;
         int[] componentSize;
         int components;
 
         UnionFind(int n) {
             components = n;
-            representative = new int[n+1];
-            componentSize = new int[n+1];
+            representative = new int[n + 1];
+            componentSize = new int[n + 1];
 
-            for(int i=0;i<=n;i++) {
+            for (int i = 0; i <= n; i++) {
                 representative[i] = i;
-                componentSize[i] = i;
+                componentSize[i] = 1;
             }
         }
 
         int findRepresentative(int x) {
-            if(representative[x] == x) {
+            if (representative[x] == x) {
                 return x;
             }
-
             return representative[x] = findRepresentative(representative[x]);
         }
 
         int preformUnion(int x, int y) {
-            x = findRepresentative(x); y = findRepresentative(y);
+            x = findRepresentative(x);
+            y = findRepresentative(y);
 
-            if(x==y) {
+            if (x == y) {
                 return 0;
             }
 
-            if(componentSize[x]>componentSize[y]) {
-                componentSize[x]+=componentSize[y];
+            if (componentSize[x] > componentSize[y]) {
+                componentSize[x] += componentSize[y];
                 representative[y] = x;
             } else {
-                componentSize[y]+=componentSize[x];
+                componentSize[y] += componentSize[x];
                 representative[x] = y;
             }
 
@@ -72,7 +72,7 @@ class Solution {
         }
 
         boolean isConnected() {
-            return components==1;
+            return components == 1;
         }
-    }    
+    }
 }
