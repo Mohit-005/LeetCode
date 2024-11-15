41ae7227f47dfbd8b9e4335445599081116db404
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-11-16 04:25:34 +0530

    Time: 2 ms (82.14%) | Memory: 65 MB (48.21%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -1,36 +1,24 @@
 class Solution {
     public int findLengthOfShortestSubarray(int[] arr) {
-        int n = arr.length;
-        
-        // Step 1: Find the longest non-decreasing prefix
-        int left = 0;
-        while (left + 1 < n && arr[left] <= arr[left + 1]) {
-            left++;
-        }
-        
-        // If the entire array is already sorted
-        if (left == n - 1) return 0;
-        
-        // Step 2: Find the longest non-decreasing suffix
-        int right = n - 1;
-        while (right > 0 && arr[right - 1] <= arr[right]) {
-            right--;
-        }
-        
-        // Step 3: Find the minimum length to remove by comparing prefix and suffix
-        int result = Math.min(n - left - 1, right);
-        
-        // Step 4: Use two pointers to find the smallest middle part to remove
-        int i = 0, j = right;
-        while (i <= left && j < n) {
-            if (arr[i] <= arr[j]) {
-                result = Math.min(result, j - i - 1);
+        int n=arr.length;
+        int left=0,right=arr.length-1;
+        while(left<n-1&&arr[left]<=arr[left+1])
+        left++;
+        if(left==n-1) return 0;
+        while(right>0&&arr[right]>=arr[right-1])
+        right--;
+        int res=Math.min(right,n-left-1);
+        int i=0;
+        while(i<=left&&right<n)
+        {
+            if(arr[i]<=arr[right])
+            {
+                res=Math.min(res,right-i-1);
                 i++;
-            } else {
-                j++;
             }
+            else
+            right++;
         }
-        
-        return result;
+        return res;
     }
 }
