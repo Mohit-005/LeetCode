a2a865111bfc1e9aa3fb1d0ccc73bc4cc06e1b63
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2024-11-19 10:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (5.02%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,23 +1,23 @@
 class Solution {
-    private int[][] dp;
-    public int uniquePaths(int m, int n) {
-        dp = new int[m + 1][n + 1];
-        for(int[] i : dp)
-            Arrays.fill(i , -1);
-        return solve(0 , 0 , m , n);
-    }
-    private int solve(int i , int j , int m , int n){
-        if(i >= m || j >= n)
+    public int dpApproach(int m,int n,Integer[][] dp,int startRow,int startColumn){
+        if(startRow==m-1 && startColumn==n-1){
+           return 1;
+        }
+        if(startRow<0 ||startColumn<0||startColumn>=n || startRow>=m)
             return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        if(i == m - 1 && j == n - 1)
-            return 1;
-        
-        int down = solve(i + 1 , j , m , n);
-        int right = solve(i , j + 1 , m , n);
-
-        return dp[i][j] = down + right;
+        if(dp[startRow][startColumn]!=null)
+            return dp[startRow][startColumn];
+        int[][]distanceMatrix={{1,0},{0,1}};
+        int uniquePath=0;
+        for(int[] dist : distanceMatrix){
+            uniquePath+=dpApproach(m,n,dp,startRow+dist[0],startColumn+dist[1]);
+        }
+         dp[startRow][startColumn]=uniquePath;
+        return uniquePath;
+    }
+    
+    public int uniquePaths(int m, int n) {
+        Integer[][] dp=new Integer[m][n];
+        return dpApproach(m,n,dp,0,0);
     }
 }
